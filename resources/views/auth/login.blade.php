8e7d590600398a1f24845870e3de3a50bdc259c4
andinaqma <andiniaqmarinap@gmail.com>
2025-12-09 17:47:09 +0700

    update register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,13 +46,13 @@
 
     <!-- LEFT -->
     <div class="bg-[#123A2D]/95 backdrop-blur-md text-white p-10 flex flex-col justify-center">
-        <h1 class="text-5xl font-bold mb-4 animate-content animate-fade-zoom delay-100">🐣 Mathify</h1>
-        <p class="animate-content animate-fade-zoom delay-200">Belajar matematika jadi lebih seru dan interaktif!</p>
+        <h1 class="text-5xl font-bold mb-4 ">🐣 Mathify</h1>
+        <p>Belajar matematika jadi lebih seru dan interaktif!</p>
     </div>
 
     <!-- RIGHT -->
     <div class="relative bg-[#f5e2c9]/30 backdrop-blur-md p-10">
-        <h2 class="text-white 4xl font-bold mb-5 nimate-content animate-fade-zoom delay-100">Login Akun</h2>
+        <h2 class="text-white 4xl font-bold mb-5 ">Login Akun</h2>
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
@@ -68,7 +68,7 @@
                     </div>
                 </div>
             @endif
-            <div class="mb-3 animate-content animate-fade-zoom delay-200">
+            <div class="mb-3">
                 <label class="text-sm font-semibold text-white">Email</label>
                 <input type="email" name="email" value="{{ old('email') }}"
                     class="w-full mt-1 px-3 py-2 rounded-lg border 
@@ -80,7 +80,7 @@
                     <p class="text-red-300 text-xs mt-1 font-semibold">{{ $message }}</p>
                 @enderror
             </div>
-            <div class="mb-3 animate-fade-zoom delay-200">
+            <div class="mb-3">
                 <label class="text-sm font-semibold text-white">Password</label>
                 <input type="password" name="password"
                     class="w-full mt-1 px-3 py-2 rounded-lg border 
@@ -93,11 +93,11 @@
                 @enderror
             </div>
 
-            <button type="submit" class="w-full bg-[#1e4233] hover:bg-[#0F2922] text-white py-2 rounded-lg font-bold transition animate-content animate-fade-zoom delay-300">
+            <button type="submit" class="w-full bg-[#1e4233] hover:bg-[#0F2922] text-white py-2 rounded-lg font-bold transition">
                 Login
             </button>
 
-            <p class="text-sm mt-4 text-white animate-content animate-fade-zoom delay-300">
+            <p class="text-sm mt-4 text-white">
                 Belum punya akun?
                 <a href="/register" class="text-[#f0da9f] hover:text-white font-bold transition">Register</a>
             </p>
